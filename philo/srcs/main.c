298a1319ad821e6d42b8326e322d2adc981d9f11
Loic Fourmaux <lfourmau@z4r2p3.42lyon.fr>
2021-07-26 14:19:49 +0200

    just add gettimeoftheday test.

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -5,13 +5,12 @@ void	*increment(void *ptr)
 	int i = 0;
 	pthread_mutex_lock(&((t_test *)ptr)->mutex);
 	printf("Increm lauched...\n");
-	while (i < 200000)
+	while (i < 100000)
 	{
 		((t_test *)ptr)->shared++;
 		i++;
 	}
 	pthread_mutex_unlock(&((t_test *)ptr)->mutex);
-	printf("Increm finished...\n");
 	return (NULL);
 }
 
@@ -20,16 +19,20 @@ int main()
 	t_test *mutex = malloc(sizeof(t_test *));
 	mutex->shared = 0;
 	mutex->res_init = pthread_mutex_init(&mutex->mutex, NULL);
-	pthread_t threads[5];
+	pthread_t threads[10];
+	struct timeval current_time;
+
+	gettimeofday(&current_time, NULL);
+	printf("%ld\n%d\n", current_time.tv_sec, current_time.tv_usec);
 
 	int i = 0;
-	while (i <= 4)
+	while (i <= 9)
 	{
 		pthread_create(&threads[i], NULL, increment, (void *)mutex);
 		i++;
 	}
 	i = 0;
-	while (i <= 4)
+	while (i <= 9)
 	{
 		pthread_join(threads[i], NULL);
 		i++;
