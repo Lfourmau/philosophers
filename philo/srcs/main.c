9c463956b427cb6645b3f607e61c03919050d7aa
Loic Fourmaux <lfourmau@z4r2p3.42lyon.fr>
2021-07-26 10:38:58 +0200

    first test with threads and mutex, with a global var

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -1,6 +1,40 @@
 #include "../includes/philo.h"
+pthread_mutex_t mutexinit = PTHREAD_MUTEX_INITIALIZER;
+int 			shared;
+
+void	*test_thread_fct(void *argp)
+{
+	int i = 0;
+	(void)argp;
+	pthread_mutex_lock(&mutexinit);
+	printf("Incrementing the bordel...\n");
+	while (i < 100000)
+	{
+		shared++;
+		i++;
+	}
+	pthread_mutex_unlock(&mutexinit);
+	return (NULL);
+}
 
 int main(int argc, char **argv)
 {
-	
+	(void)argc;
+	(void)argv;
+
+	pthread_t threads[5];
+	int i = 0;
+	while (i <= 4)
+	{
+		pthread_create(&threads[i], NULL, test_thread_fct, NULL);
+		i++;
+	}
+	i = 0;
+	while (i <= 4)
+	{
+		pthread_join(threads[i], NULL);
+		i++;
+	}
+	printf("%d\n", shared);
+	return (0);
 }
